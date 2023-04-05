b99e2d609ddd6b28c50f7bedd647e606c7dba758
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-04-05 10:59:30 +0530

    updated based on feedback

--- hello-react/src/index.tsx
+++ hello-react/src/index.tsx
@@ -17,11 +17,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
-function onPerfEntry(perf) {
-  // Handle the performance data
-  console.log(perf);
-}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(onPerfEntry);
+reportWebVitals();
