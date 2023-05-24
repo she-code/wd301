c510fe5ec256b9cffef0f5ecc9530e8b2d484032
Frehiwot Haile <54357776+she-code@users.noreply.github.com>
2023-05-24 18:57:26 +0530

    added a minor change based on feedback

--- smarter-tasks/src/NotFound.tsx
+++ smarter-tasks/src/NotFound.tsx
@@ -5,7 +5,12 @@ export default function NotFound() {
   return (
     <div className=" shadow-lg mx-auto w-2/4 my-5  h-1/3 p-5">
       <p className="mb-5 text-xl">404 Page Not Found</p>
-      <Link to="/" className="ml-6 text-white bg-green-600 px-3 py-2">
+      <Link
+        to="/"
+        role="button"
+        className="ml-6 text-white bg-green-600 px-3 py-2"
+        id="backToHomeButton"
+      >
         Home
       </Link>
     </div>
